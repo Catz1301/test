2624d2c71b555f001303dea4f001699bb0878463
Catz1301 <millerj22@charlottenet.org>
2021-11-07 22:48:26 -0500

    Added things

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: "InspirobotBatch",
+    msTileColor: "#007515",
+    manifestCrossorigin: "anonymous",
+    manifestOptions: {
+      background_color: "#007515",
+    },
+  },
+};
